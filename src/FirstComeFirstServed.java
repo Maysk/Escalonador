c0f2a0caf4eaeb99abe1774e7aaf45fba7dd632e
Caio Viktor Avila <caioviktor@hotmail.com>
2015-11-27 17:04:55 -0300

    Correção Bug prioridade

--- src/FirstComeFirstServed.java
+++ src/FirstComeFirstServed.java
@@ -9,11 +9,9 @@ public class FirstComeFirstServed extends StrategyEscalonador{
     public FirstComeFirstServed(ArrayList<Processo> processos){
         processosEstadoPronto = new TreeSet<>();
         this.processos = processos;
-        int ordem = 0;
         for(Processo processo : processos){
-            processo.setPrioridade(ordem);
+            processo.setPrioridade(processo.getTempoChegada());
             processosEstadoPronto.add(processo);
-            ordem++;
         }
         tempoCorrente = 0;
     }
